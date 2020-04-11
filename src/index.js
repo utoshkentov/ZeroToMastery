6272ccf57c492c78c06aa89ac494245af854e5ca
Umrzoq-8855 <60574959+Umrzoq-8855@users.noreply.github.com>
2020-04-12 00:20:24 +0500

    I have added Redux Persistor into my application which helps for staying my changes after uploadig page

--- src/index.js
+++ src/index.js
@@ -2,18 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from 'react-redux';
+import {PersistGate} from "redux-persist/integration/react";
+
+import {store, persistor} from "./redux/store";
 
 import './index.css';
 import App from './App';
 
-import store from "./redux/store";
 import ParticlesBg from "particles-bg";
 
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
-            <ParticlesBg type="cobweb" bg={true} style={{width: '100%', height: '100%'}}/>
-            <App/>
+            <PersistGate persistor={persistor}>
+                <ParticlesBg type="cobweb" bg={true} style={{width: '100%', height: '100%'}}/>
+                <App/>
+            </PersistGate>
         </BrowserRouter>
     </Provider>,
     document.getElementById('root')
