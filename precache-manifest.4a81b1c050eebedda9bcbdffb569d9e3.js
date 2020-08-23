c737526cb1f5008c5225dd9c19e72075a9437379
Umrzoq Toshkentov <utoshkentov@gmail.com>
2020-08-23 20:06:08 +0500

    Updates

---
+++ precache-manifest.4a81b1c050eebedda9bcbdffb569d9e3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd69063c17568db09d9e33f5a564c505",
+    "url": "/ZeroToMastery/index.html"
+  },
+  {
+    "revision": "f1b3c126d52a16180056",
+    "url": "/ZeroToMastery/static/css/main.af598a50.chunk.css"
+  },
+  {
+    "revision": "61b178ebf5afbe4125eb",
+    "url": "/ZeroToMastery/static/js/2.8170bda5.chunk.js"
+  },
+  {
+    "revision": "6eff4c09931cc443357c20788b57bf96",
+    "url": "/ZeroToMastery/static/js/2.8170bda5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1b3c126d52a16180056",
+    "url": "/ZeroToMastery/static/js/main.6a0f696b.chunk.js"
+  },
+  {
+    "revision": "089e7ff92d7878f18e31",
+    "url": "/ZeroToMastery/static/js/runtime-main.98e36639.js"
+  },
+  {
+    "revision": "fffc359c60edd4c2988fbecb8a3620d7",
+    "url": "/ZeroToMastery/static/media/animal.fffc359c.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/ZeroToMastery/static/media/shopping-bag.30f07071.svg"
+  }
+]);
